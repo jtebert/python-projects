d161d5dac6b9192dc37e59e87ac574be8d983da1
Julia Ebert <ebertjulia@gmail.com>
2014-05-09 13:38:01 -0400

    Minor fixes Changed word frequency graph to use percent occurrence Fixed bin sizing for word distribution search

--- word_counter/Word_counter.py
+++ word_counter/Word_counter.py
@@ -50,19 +50,19 @@ class Word_counter:
             str(self.get_percent(pair[1])) + "%)"
         
     # Plot the distribution of word frequency (for all words >.1% of total num)
-    # TODO : Change y-axis to % instead of number of occurrences
     def plot_words_freq(self):
         thresh_num = self.num_strings * .1/100.0
         num_occurrences = self.word_map.values()
         num_occurrences.sort()
         num_occurrences.reverse()
         num_occurrences = filter(lambda x: x > thresh_num, num_occurrences)
+        num_occurrences = [self.get_percent(x) for x in num_occurrences]
         ind = range(len(num_occurrences))
         fig, ax = plt.subplots()
         rects = ax.bar(ind, num_occurrences, width=1, color='b',edgecolor='none')
         #Add labels, etc.
         ax.set_title('Distribution of Word Commonality')
-        ax.set_ylabel('Number of Occurrences')
+        ax.set_ylabel('Percentage of Use')
         ax.set_xlabel('Word Usage Order')
         ax.set_xlim(xmax=len(num_occurrences))
         plt.draw()
@@ -71,9 +71,9 @@ class Word_counter:
     # Current simple version: basically a histogram
     def word_xray(self, word):
         word = word.lower()
-        #num_text_divs = 500
-        #text_div_size = self.num_strings / num_text_divs
-        text_div_size = 250
+        num_text_divs = 500
+        text_div_size = self.num_strings / num_text_divs
+        #text_div_size = 100
         text_divs = range(0, self.num_strings, text_div_size)
         num_occurrences = []
         for i in text_divs:
